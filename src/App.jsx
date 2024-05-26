018ab1e59338d58c2efb3874d986255b1cc91adc
Roy <royrmagdaong@gmail.com>
2024-05-26 17:06:31 +0800

    lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@
 import './App.css'
 // import MyComponent from './components/my-component'
 // import Counter from './components/count'
-import Login from './components/login'
+// import Login from './components/login'
+import Child from './components/child'
+import { useState } from 'react'
 
 function App() {
+  const [name, setName] = useState('test')
+
+  const printEmail = (email) => {
+    console.log("email", email)
+  }
 
   return (
     <>
       {/* <MyComponent name="Roy" age={12}/>
       <MyComponent name="John" age={13} address="pasig city"/> */}
       {/* <Counter /> */}
-      <Login />
+      <Child name={name} printEmail={printEmail}/>
     </>
   )
 }
